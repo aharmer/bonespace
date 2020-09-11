9b4ca74eac97420b6d475cfafb550244773dc106
aharmer <aharmer@gmail.com>
2020-09-11 14:17:57 +1200

    remesh function generated

--- R/remesh.R
+++ R/remesh.R
@@ -1,21 +1,16 @@
-library(rgl)
-library(Rvcg)
-
-hum = vcgImport("/Users/aharmer/Downloads/hum.obj", updateNormals = TRUE, readcolor = FALSE, clean = TRUE, silent = FALSE)
-hum_inst = vcgImport("/Users/aharmer/Downloads/hum_inst.obj", updateNormals = TRUE, readcolor = FALSE, clean = TRUE, silent = FALSE)
-
-remesh = function(mesh, scale) {
-  edge.length = as.numeric(vcgMeshres(mesh)[1])
-  new.mesh = vcgUniformRemesh(mesh, voxelSize = edge.length*(1/sqrt(scale)), multiSample = TRUE)
-  smooth.mesh = vcgSmooth(new.mesh, type = "surfPreserveLaplace", iteration = 1, delta = 0.3)
-  return(smooth.mesh)
+#' Remesh and decimate mesh objects
+#'
+#' This function remeshes a list of mesh3d objects and decimates the number of vertices if required.
+#' @param mesh.list a list of mesh3d objects.
+#' @param scale the relative scale (only approximate) of remeshed objects. Values less than one have reduced number of vertices relative to original mesh.
+#' @return A list containing the uniform remeshed (and decimated) objects.
+#' @importFrom Rvcg rgl
+#' @export
+remesh = function(mesh.list, scale) {
+  rem = lapply(mesh.list, function(mesh, scale){
+    edge.length = as.numeric(vcgMeshres(mesh)[1])
+    new.mesh = vcgUniformRemesh(mesh, voxelSize = edge.length*(1/sqrt(scale)), multiSample = TRUE)
+    smooth.mesh = vcgSmooth(new.mesh, type = "surfPreserveLaplace", iteration = 1, delta = 0.3)
+  }, scale)
+  return(rem)
 }
-
-
-hum.rem = remesh(hum, 0.1)
-meshInfo(hum.rem)
-open3d() + shade3d(hum.rem, color = "lightblue")
-
-
-vcgObjWrite(hum, "~/Downloads/humerus.obj")
-vcgPlyWrite(hum, "~/Downloads/humerus.ply")
